88e39a1fa551d8fc303d0f70307df945634143dd
Antto Sinkko <antto.sinkko@student.tut.fi>
2017-04-20 00:23:23 +0300

    Made the WS1 agent a bit more explicit and added unload paper function

--- WS1_Agent.js
+++ WS1_Agent.js
@@ -8,7 +8,7 @@ var request = require('request');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); // Body parser uses JSON data
+app.use(bodyParser.json());
 
 var port = 6001;
 
@@ -17,41 +17,61 @@ function subscribe() {
 
     // get the notification when the pallet has moved to the Z1 in CNV1
     request.post('http://localhost:3000/RTU/SimCNV1/events/Z1_Changed/notifs',
-        {form: {destUrl: "http://localhost:4806"}}, function (err, httpResponse, body) {
+        {form:{destUrl:"http://localhost:" + port}}, function(err,httpResponse, body){
             if (err) {
                 console.log(err);
             } else {
-                console.log("subscribed to the Z1 changed in station 1!");
+                console.log("Subscribed to the Z1 changed in CNV1!");
             }
         });
 
     // get the notification when the pallet has moved to the Z2 in CNV1
     request.post('http://localhost:3000/RTU/SimCNV1/events/Z2_Changed/notifs',
-        {form: {destUrl: "http://localhost:" + port}}, function (err, httpResponse, body) {
+        {form:{destUrl:"http://localhost:" + port}}, function(err, httpResponse, body) {
             if (err) {
                 console.log(err);
             } else {
-                console.log("subscribed to the Z2 changed in station 1!");
+                console.log("subscribed to the Z2 changed in CNV1!");
             }
         });
 
     // get the notification when the pallet has moved to the Z3 in CNV1
     request.post('http://localhost:3000/RTU/SimCNV1/events/Z3_Changed/notifs',
-        {form: {destUrl: "http://localhost:" + port}}, function (err, httpResponse, body) {
+        {form:{destUrl:"http://localhost:" + port}}, function(err, httpResponse, body) {
             if (err) {
                 console.log(err);
             } else {
-                console.log("subscribed to the Z3 changed in station 1!");
+                console.log("subscribed to the Z3 changed in CNV1!");
             }
         });
 
     // get the notification when the pallet has moved to the Z5 in CNV1
     request.post('http://localhost:3000/RTU/SimCNV1/events/Z5_Changed/notifs',
-        {form: {destUrl: "http://localhost:" + port}}, function (err, httpResponse, body) {
+        {form:{destUrl:"http://localhost:" + port}}, function(err, httpResponse, body) {
             if (err) {
                 console.log(err);
             } else {
-                console.log("subscribed to the Z5 changed in station 1!");
+                console.log("subscribed to the Z5 changed in CNV1!");
+            }
+        });
+
+    // get the notification when the paper is loaded to the pallet
+    request.post('http://localhost:3000/RTU/SimROB7/events/PalletLoaded/notifs',
+        {form:{destUrl:"http://localhost:" + port}}, function(err, httpResponse, body) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("subscribed to the pallet loaded!");
+            }
+        });
+
+    // get the notification when the paper is unloaded to the pallet
+    request.post('http://localhost:3000/RTU/SimROB7/events/PalletUnloaded/notifs',
+        {form:{destUrl:"http://localhost:" + port}}, function(err, httpResponse, body) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("subscribed to the pallet unloaded!");
             }
         });
 }
@@ -74,6 +94,28 @@ function move(zone, station) {
     });
 }
 
+// A function that loads the paper to the pallet
+function loadPaper() {
+    request.post('http://localhost:3000/RTU/SimROB1/services/LoadPaper',
+        {form: {destUrl: "http://localhost:" + port}}, function (err, httpResponse, body) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Paper loaded!");
+            }
+        });
+}
+
+function unloadPaper() {
+    request.post('http://localhost:3000/RTU/SimROB1/services/UnloadPallet',
+        {form: {destUrl: "http://localhost:" + port}}, function (err, httpResponse, body) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Paper unloaded!");
+            }
+        });
+}
 
 
 // Takes the POST requests
@@ -81,9 +123,9 @@ app.post('/', function(req, res) {
     // Shows that we post now
     console.log("Got POST request that is: ");
     console.log(req.body);
-    console.log(req.payload);
 
 
+    // Ifs for moving the pallet to the paper loading
     if (req.body.senderID === 'SimCNV1') {
         if (req.body.id === 'Z1_Changed' && req.body.payload.PalletID !== -1) {
             move(12,1);
@@ -92,19 +134,22 @@ app.post('/', function(req, res) {
             move(23,1);
         }
         if (req.body.id === 'Z3_Changed' && req.body.payload.PalletID !== -1) {
-            request.post('http://localhost:3000/RTU/SimROB1/services/LoadPaper',
-                {form: {destUrl: "http://localhost:" + port}}, function (err, httpResponse, body) {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log("Paper loaded!");
-                    }
-                });
+            loadPaper();
+        }
+    }
+    // If for moving the pallet from the paper loading
+    if (req.body.senderID === 'ROB1') {
+        if (req.body.id === 'PaperLoaded') {
+            move(35,1);
         }
     }
 
+    // tää on hyvä laittaa, muuten saattaa tulla timeoutteja yms kun lähettäjä odottelee vastauksen loppua
+    res.end('post ok');
+
 })
 
+// Calls the subscribe function
 subscribe();
 
 // Start listening
